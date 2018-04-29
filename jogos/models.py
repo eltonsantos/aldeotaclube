24231780546ae02d00894bce652b7be2489f98d8
Elton Santos <eltonaxl@hotmail.com>
2018-04-29 00:02:44 -0300

    Commit more changes

--- jogos/models.py
+++ jogos/models.py
@@ -4,8 +4,11 @@ class Jogo(models.Model):
     adversario = models.CharField(max_length=50)
     data = models.DateField()
     local = models.CharField(max_length=50)
-    arbitragem = models.CharField(max_length=80)
+    arbitragem = models.CharField(max_length=80, null=True, blank=True)
     escudo_adversario = models.ImageField(upload_to='jogos', null=True, blank=True)
+    placar_aldeota = models.CharField(max_length=10, null=True, blank=True)
+    placar_adversario = models.CharField(max_length=10, null=True, blank=True)
+    resumo = models.TextField()
 
     def __str__(self):
         return self.adversario
