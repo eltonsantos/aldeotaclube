fc2eecf1632b7a7f9696891f7bc8ccf482e2ab09
Elton Santos <eltonaxl@hotmail.com>
2018-05-21 01:32:41 -0300

    Commiting all

--- aldeotaclube/settings.py
+++ aldeotaclube/settings.py
@@ -149,7 +149,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.s3util.StaticStorage'
+DEFAULT_FILE_STORAGE = 'storages.s3util.MediaStorage'
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
